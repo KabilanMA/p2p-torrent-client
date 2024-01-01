3124f21639ed40231271b5000c579d75e280df30
KabilanMA <kabilanen@gmail.com>
2024-01-01 09:20:01 +0530

    First working prototype

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/KabilanMA/p2p-torrent-client
+
+go 1.21.5
+
+require github.com/jackpal/bencode-go v1.0.2
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/KabilanMA/p2p-torrent-client/torrentfile"
+)
+
+func main() {
+	inPath := os.Args[1]
+	outPath := os.Args[2]
+
+	tf, err := torrentfile.Open(inPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = tf.DownloadToFile(outPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
